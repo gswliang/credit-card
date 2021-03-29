2cfa8d33615a853d8e2826b3f91f50f956832165
Grace <gswliang@gmail.com>
2021-03-29 15:52:01 +0800

    add search input

---
+++ src/components/Input.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Input.css";
+
+const Input = ({ number, setNumber }) => {
+  console.log(number);
+  return (
+    <div className="input__container">
+      <label className="input__label">Enter a number:</label>
+      <input
+        className="input__content"
+        value={number}
+        onChange={(e) => {
+          setNumber(e.target.value);
+        }}
+      />
+    </div>
+  );
+};
+
+export default Input;
