a4eea78a2611946cb1ce8a5b8833979fccc7c627
Terry Liao <terry.liao@nogle.com>
2021-04-01 17:20:35 +0800

    optimize code

--- src/components/Cardtypes.js
+++ src/components/Cardtypes.js
@@ -1,19 +1,14 @@
 import React from "react";
 import "./Input.css";
 
-const Cardtypes = ({ cardOptions, setCardselected }) => {
+const Cardtypes = ({ cardOptions, onChange }) => {
   const renderedOptions = cardOptions.map((card) => {
-    return <option key={card.label}>{card.label}</option>;
+    return <option key={card}>{card}</option>;
   });
   return (
     <div className="input__container">
       <label className="input__label">Card type:</label>
-      <select
-        className="input__content selection"
-        onChange={(e) => {
-          setCardselected(e.target.value);
-        }}
-      >
+      <select className="input__content selection" onChange={onChange}>
         {renderedOptions}
       </select>
     </div>
--- src/components/CreditCardList.js
+++ src/components/CreditCardList.js
@@ -6,32 +6,15 @@ import "./creditCardList.css";
 import useDebounce from "../hooks/useDebounce";
 import Cardtypes from "./Cardtypes";
 
-const cardOptions = [
-  {
-    label: "Visa",
-  },
-  {
-    label: "American Express",
-  },
-  {
-    label: "MasterCard",
-  },
-  {
-    label: "Visa Retired",
-  },
-  {
-    label: "Discover Card",
-  },
-];
-
 const DEBOUNCE_DELAY = 500;
 
 const CreditCardList = () => {
   const [cards, setCards] = useState([]);
   const [number, setNumber] = useState(0);
   const [isNum, setIsNum] = useState(true);
-  const [cardSelected, setCardselected] = useState(cardOptions[0].label);
+  const [cardSelected, setCardSelected] = useState("All");
   const debounceInput = useDebounce(number, DEBOUNCE_DELAY);
+  const [cardTypes, setCardTypes] = useState([]);
 
   const isValidInput = (number) => {
     if (0 <= number && number <= 1000) {
@@ -48,22 +31,33 @@ const CreditCardList = () => {
     getCreditCards(debounceInput)
       .then((res) => {
         setCards(res.data.data);
+        setCardTypes(getCardTypes(res.data.data));
       })
       .catch((e) => {})
       .finally(() => {});
   }, [debounceInput]);
 
   const filteredCard = cards.filter((card) => {
-    return card.type === cardSelected;
+    return cardSelected === "All" ? card : card.type === cardSelected;
   });
+
+  const getCardTypes = (cards) => {
+    const cardTypes = cards.map((card) => {
+      return card.type;
+    });
+    return ["All", ...new Set(cardTypes)];
+  };
 
   return (
     <div>
       <Input number={number} setNumber={setNumber} isNum={isNum} />
-      <Cardtypes cardOptions={cardOptions} setCardselected={setCardselected} />
-      <div className="credit-card__warning">
-        {isNum ? " " : "Please enter a value between 1 - 1000"}
-      </div>
+      <Cardtypes
+        cardOptions={cardTypes}
+        onChange={(e) => {
+          setCardSelected(e.target.value);
+        }}
+      />
+      <div className="credit-card__warning">{isNum ? " " : "Please enter a value between 1 - 1000"}</div>
       <div className="credit-card-list">
         {filteredCard.map((card) => (
           <CreditCard key={card.number} type={card} />
